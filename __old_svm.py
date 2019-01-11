b142db576ad99304a29dea6d182873e5f91ef4cd
kunnst18 <kunnst18@b042-18.crio.dauphine.fr>
2019-01-11 17:11:11 +0100

    change tous les noms

---
+++ __old_svm.py
@@ -0,0 +1,91 @@
+import numpy as np
+from scipy import optimize
+
+def process_one_line(line):
+  words = line.split(',')
+  d = len(words) - 1
+  x = [float(word) for word in words]
+  y = x[-1]
+  y = 1 if y == 1 else -1
+  x = x[:-1]
+  return (d, x, y)
+
+def calculeLine(x, y):
+  xlst = [- y * xk for xk in x]
+  xlst.append(-y)
+  return xlst
+
+def addConstraintsForB(Alst, d):
+  c1 = [0 for i in range(d)]
+  c1[-1] = 1
+  c2 = [0 for i in range(d)]
+  c2[-1] = -1
+  Alst.append(c1)
+  Alst.append(c2)
+
+def load_data(filename):
+  with open(filename, 'r') as f:
+    n = 0
+    Alst = []
+    for line in f:
+      line = line.strip()
+      old_d = None
+      if len(line) > 3 and '@' not in line:
+        d, x, y = process_one_line(line)
+        if old_d is not None and d != old_d:
+          print("\n\n\nLINE OF DIFFERENT SIZES ", old_d, " AND ", d, "\n\n\n")
+          exit(-1)
+        Alst.append(calculeLine(x, y))
+        n = n + 1
+        old_d = d
+    addConstraintsForB(Alst, d + 1)
+    A = np.array([[cell for cell in line] for line in Alst], ndmin = 2)
+    return (n, d, A)
+
+#filename = 'data/bc-orig.txt'
+filename = 'data/test_bc_orig.txt'
+(n, d, A) = load_data(filename)
+minusOne = np.array([-1 for i in range(n+2)])
+
+#print(A[:10])
+#print('...')
+#print(A[-10:])
+
+w0 = np.random.randn(d+1)
+
+# objective: sum w_i^2 (sauf le dernier coeff de w qui est le biais)
+def objective(w):
+  return (np.dot(w.T, w) - w[-1] * w[-1])
+
+# jac: sum w_i (sauf le dernier)
+#def jac(w):
+#  d = w.size
+#  coeffs = [1 for i in range(d)]
+#  coeffs[-1] = 0
+#  return (np.dot(w.T, np.array(coeffs)))
+  
+def jac(w):
+  d = w.size
+  coeffs = np.eye(d)
+  coeffs[-1,-1] = 0
+  return (np.dot(coeffs, w))
+
+cons = {'type':'ineq',
+        'fun':lambda w: minusOne - np.dot(A,w),
+        'jac':lambda w: -A}
+
+opt = {'disp':False}
+
+def solve(objective, jac, cons):
+
+    res_cons = optimize.minimize(objective, w0, jac=jac, constraints=cons,
+                                 method='SLSQP', options=opt)
+
+    #res_uncons = optimize.minimize(loss, x0, jac=jac, method='SLSQP',
+    #                               options=opt)
+
+    print('\nResults:')
+    print(res_cons)
+
+if __name__ == '__main__':
+  solve(objective, jac, cons)
--- svm.py
+++ svm.py
@@ -1,91 +1,38 @@
-import numpy as np
-from scipy import optimize
+import cplex
 
 def process_one_line(line):
-  words = line.split(',')
-  d = len(words) - 1
-  x = [float(word) for word in words]
-  y = x[-1]
-  y = 1 if y == 1 else -1
-  x = x[:-1]
+  numbers = [float(word) for word in line.strip().split(',')]
+  y = 1.0 if numbers[-1] == 1 else -1.0
+  x = numbers[:-1]
+  d = len(x)
   return (d, x, y)
-
-def calculeLine(x, y):
-  xlst = [- y * xk for xk in x]
-  xlst.append(-y)
-  return xlst
-
-def addConstraintsForB(Alst, d):
-  c1 = [0 for i in range(d)]
-  c1[-1] = 1
-  c2 = [0 for i in range(d)]
-  c2[-1] = -1
-  Alst.append(c1)
-  Alst.append(c2)
 
 def load_data(filename):
   with open(filename, 'r') as f:
-    n = 0
-    Alst = []
+    X = []
+    y = []
     for line in f:
-      line = line.strip()
-      old_d = None
       if len(line) > 3 and '@' not in line:
-        d, x, y = process_one_line(line)
-        if old_d is not None and d != old_d:
-          print("\n\n\nLINE OF DIFFERENT SIZES ", old_d, " AND ", d, "\n\n\n")
-          exit(-1)
-        Alst.append(calculeLine(x, y))
-        n = n + 1
-        old_d = d
-    addConstraintsForB(Alst, d + 1)
-    A = np.array([[cell for cell in line] for line in Alst], ndmin = 2)
-    return (n, d, A)
+        (d, xi, yi) = process_one_line(line)
+        X.append(xi)
+        y.append(yi)
+#    X = np.array(X)
+#    y = np.array(y)
+    return (X, y)
 
-#filename = 'data/bc-orig.txt'
-filename = 'data/test_bc_orig.txt'
-(n, d, A) = load_data(filename)
-minusOne = np.array([-1 for i in range(n+2)])
+def createCplexInstance(d,n,X,y):
+  p = cplex.Cplex()
+  p.set_problem_name("SVM")
+  p.objective.set_sense(p.objective.sense.minimize)
+  my_colnames = [["w" + str(i) for i in range(d)], ["b"],
+                        ["z" + str(i) for i in range(n)]]
+  p.variables.add(types = [p.variables.type.continuous] * len(my_colnames[0]),
+                  names = my_colnames[0])
+  p.objective.set_quadratic([[[i],[1]] for i in range(d)])
+  p.variables.add(obj=[0], types = p.variables.type.continuous, names="b")
+  wxiplusb_left = createWxiplusbConstraints(d,n,X,y)
+  p.linear_constraints.add(lin_expr=wxiplusb_left,
+                           senses=['G' for i in range(d)],
+                           rhs=wxiplusb_right)
+  return p
 
-#print(A[:10])
-#print('...')
-#print(A[-10:])
-
-w0 = np.random.randn(d+1)
-
-# objective: sum w_i^2 (sauf le dernier coeff de w qui est le biais)
-def objective(w):
-  return (np.dot(w.T, w) - w[-1] * w[-1])
-
-# jac: sum w_i (sauf le dernier)
-#def jac(w):
-#  d = w.size
-#  coeffs = [1 for i in range(d)]
-#  coeffs[-1] = 0
-#  return (np.dot(w.T, np.array(coeffs)))
-  
-def jac(w):
-  d = w.size
-  coeffs = np.eye(d)
-  coeffs[-1,-1] = 0
-  return (np.dot(coeffs, w))
-
-cons = {'type':'ineq',
-        'fun':lambda w: minusOne - np.dot(A,w),
-        'jac':lambda w: -A}
-
-opt = {'disp':False}
-
-def solve(objective, jac, cons):
-
-    res_cons = optimize.minimize(objective, w0, jac=jac, constraints=cons,
-                                 method='SLSQP', options=opt)
-
-    #res_uncons = optimize.minimize(loss, x0, jac=jac, method='SLSQP',
-    #                               options=opt)
-
-    print('\nResults:')
-    print(res_cons)
-
-if __name__ == '__main__':
-  solve(objective, jac, cons)
